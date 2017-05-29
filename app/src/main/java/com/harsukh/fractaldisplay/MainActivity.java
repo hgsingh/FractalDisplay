327c6b5d794691baa408c5cabdca6b944a813e04
Harsukh Singh <harsukh.singh@gm.com>
2017-05-29 18:48:42 -0400

    adding listener to seeker

--- app/src/main/java/com/harsukh/fractaldisplay/MainActivity.java
+++ app/src/main/java/com/harsukh/fractaldisplay/MainActivity.java
@@ -2,12 +2,16 @@ package com.harsukh.fractaldisplay;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new CustomJNIView(this));
+        setContentView(R.layout.activity_main);
+        FrameLayout frameLayout = (FrameLayout) findViewById(R.id.jni_container);
+        frameLayout.addView(new CustomJNIView(this));
+
     }
 }
